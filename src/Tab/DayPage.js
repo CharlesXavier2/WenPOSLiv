397051903398bbf69678d53b5c153bbc0bd18fa3
rajnishrajput <rajkrsrajput@gmail.com>
2019-01-09 13:16:44 -0600

    ios bug resolved

--- src/Tab/DayPage.js
+++ src/Tab/DayPage.js
@@ -329,7 +329,7 @@ export default class DayPage extends Component {
                         urlPan = 'http://115.112.224.200:3000/v2/' + urlValue;
 
                     } else {
-                        urlPan = 'http:///115.112.224.200:3000/v2/get_pan_level_sale?filter_type=' + filter_type + '&date=' + urlPanDate;
+                        urlPan = 'http://115.112.224.200:3000/v2/get_pan_level_sale?filter_type=' + filter_type + '&date=' + urlPanDate;
 
 
                     }
